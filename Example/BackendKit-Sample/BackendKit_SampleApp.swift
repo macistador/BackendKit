fef774e8d00b530fcd242ad60fa92522e6ef8fbf
Michel-André Chirita <michel.chirita@gmail.com>
2024-10-14 20:49:47 +0200

    first commit

---
+++ Example/BackendKit-Sample/BackendKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackendKit_SampleApp.swift
+//  BackendKit-Sample
+//
+//  Created by Michel-Andre Chirita on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BackendKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
